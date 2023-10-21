ff2cd8d90c1edb17f2172f7d6a3e8180be137296
Вікторія <viktoriya23071@gmail.com>
2023-10-21 23:36:49 +0300

    s

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
+import React from 'react';
+import Container from './Container';
+import ContactForm from './ContactForm/ContactForm';
+import ContactsList from './ContactsList';
+import Filter from './Filter';
+import { MainTitle, SecondTitle } from './Container/Container.styled';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Container>
+      <MainTitle>Phonebook</MainTitle>
+      <ContactForm />
+      <SecondTitle>Contacts</SecondTitle>
+      <Filter />
+      <ContactsList />
+    </Container>
   );
 };
